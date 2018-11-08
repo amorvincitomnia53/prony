2bdf362506c8eebd71b6269e78a28603a92bbed1
Contramundum53 <contramundum53@outlook.com>
2018-11-08 17:41:34 +0900

    できた

--- calc.hpp
+++ calc.hpp
@@ -18,14 +18,22 @@ struct Dipole {
     }
 };
 constexpr double PI = 3.1415926535897932384626;
-double threshold = 1e-7;
+//double threshold = 1e-1;
+double log_threshold = -2;
 
+constexpr double ln10 = std::log(10.0);
 
+std::complex<double> calcPotential(const Dipole& d, const std::complex<double> z)
+{
+    if (d.m == 0.0)
+        return 0.0;
+    return (1.0 / (2 * PI)) * d.m / (z - d.z);
+}
 std::complex<double> calcPotential(const std::vector<Dipole>& dipoles, const std::complex<double> z)
 {
     std::complex<double> ret = 0;
     for (auto& d : dipoles) {
-        ret += (1.0 / (2 * PI)) * d.m / (z - d.z);
+        ret += calcPotential(d, z);
     }
     return ret;
 }
@@ -38,7 +46,7 @@ std::complex<double> calcField(const std::vector<Dipole>& dipoles, const std::co
             continue;
         std::complex<double> n = d.m / std::abs(d.m);
         auto rel = (z - d.z) / n;
-        ret += (1.0 / (2 * PI)) * d.m / std::conj(rel * rel);
+        ret += (1.0i / (2 * PI)) * d.m / std::conj(rel * rel);
     }
     return ret;
 }
@@ -92,7 +100,7 @@ std::vector<Dipole> estimate(const std::vector<Entry>& data, int pow_max)
     }
     auto svd = matrix.jacobiSvd(Eigen::ComputeThinU | Eigen::ComputeThinV);
 
-    svd.setThreshold(threshold);
+    svd.setThreshold(std::exp(ln10 * log_threshold));
     int r = svd.rank();
 
     Eigen::MatrixXcd matrix2(n, n);
@@ -109,46 +117,57 @@ std::vector<Dipole> estimate(const std::vector<Entry>& data, int pow_max)
 
 
     std::sort(eig.data(), eig.data() + n, [](const std::complex<double>& a, const std::complex<double>& b) { return std::abs(a) > std::abs(b); });
-    //    for (int i = 0; i < r; i++) {
-    //        if (std::abs(eig(i)) > 1.0) {
-    //            std::swap(eig(i), eig(--r));
-    //        }
-    //    }
+    for (int i = 0; i < r; i++) {
+        if (std::abs(eig(i)) > 1.0) {
+            std::swap(eig(i), eig(--r));
+        }
+    }
 
     if (r == 0) {
         return {};
     }
 
-    //        Eigen::MatrixXd example(data.size(), r);
-    //        for (int i = 0; i < data.size(); i++) {
-    //            for (int j = 0; j < r; j++) {
-    //                Dipole d1 = {eig[j], 1.0};
-    //                example(i, j) = potentialTable({d1, d1.conjugate()})
-    //            }
-    //        }
-    Eigen::MatrixXcd vandermonde(c.size(), r);
-    for (int i = 0; i < r; i++) {
-        std::complex<double> z = eig(i);
-        std::complex<double> s = 1;
-        for (int j = 0; j < c.size(); j++) {
-            vandermonde(j, i) = s;
-            s *= z;
+    Eigen::MatrixXd example(data.size(), r * 2);
+    Eigen::VectorXd data_vector(data.size());
+    for (int i = 0; i < data.size(); i++) {
+        for (int j = 0; j < r; j++) {
+            auto try_p = [&](const std::complex<double>& m) {
+                Dipole d11 = {eig[j], m};
+                Dipole d12 = d11.conjugate();
+                return calcPotential(d11, data[i].z).imag() + calcPotential(d12, data[i].z).imag();
+            };
+            example(i, j * 2) = try_p(1.0);
+            example(i, j * 2 + 1) = try_p(1.0i);
         }
+        data_vector(i) = data[i].p.imag();
     }
+
+    Eigen::VectorXd sol = example.fullPivHouseholderQr().solve(data_vector);
+    //    Eigen::MatrixXcd vandermonde(c.size(), r);
+    //    for (int i = 0; i < r; i++) {
+    //        std::complex<double> z = eig(i);
+    //        std::complex<double> s = 1;
+    //        for (int j = 0; j < c.size(); j++) {
+    //            vandermonde(j, i) = s;
+    //            s *= z;
+    //        }
+    //    }
     //    std::cout << vandermonde << std::endl;
 
-
-    Eigen::Matrix<std::complex<double>, Eigen::Dynamic, 1> vec(c.size());
+    /*
+    Eigen::Matrix<std::complex<double>, Eigen::Dynamic, 1>
+        vec(c.size());
     for (int i = 0; i < n; i++) {
         vec[i] = c[i];
     }
 
     Eigen::Matrix<std::complex<double>, Eigen::Dynamic, 1> m = vandermonde.jacobiSvd(Eigen::ComputeThinU | Eigen::ComputeThinV).solve(vec);
+*/
 
     //    std::cout << vec << std::endl;
     std::vector<Dipole> ret(r);
     for (int i = 0; i < r; i++) {
-        ret[i].m = m(i);
+        ret[i].m = {sol(2 * i), sol(2 * i + 1)};
         ret[i].z = eig(i);
     }
 
--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <glibmm/main.h>
+#include <gtkmm/adjustment.h>
+#include <gtkmm/builder.h>
 #include <gtkmm/glarea.h>
+#include <gtkmm/grid.h>
 #include <gtkmm/window.h>
 
 
@@ -14,18 +17,30 @@
 
 #include <array>
 #include <chrono>
+#include <gtkmm/button.h>
+#include <gtkmm/togglebutton.h>
+#include <mutex>
 #include <random>
 //#include <oglplus/math/matrix.hpp>
 using namespace std::complex_literals;
 
 
 constexpr int field_division_half = 25;
-constexpr double field_range_half = 1.5;
+constexpr double field_range_half = 1.2;
 constexpr double tick = field_range_half / field_division_half;
 
 
-int division = 500;
+int division = 50;
+int max_pow = 25;
 
+
+double log_noise = -2;
+
+
+double voltage_range = 2.0;
+
+bool active = true;
+bool running = true;
 
 int main()
 {
@@ -57,14 +72,62 @@ int main()
 
     std::vector<Dipole> dipoles{{0.00 /*1*/, 1}, {0.00 /*1*/, -1}};
     std::vector<Dipole> estimated;
+    std::vector<Entry> data;
 
     std::random_device rd;
     std::mt19937 rng(rd());
     std::normal_distribution distrib;
 
+    auto draw = [&] {
+        auto vertex = [](std::complex<double> z, double r, double g, double b) {
+            return Shader::Vertex{oglplus::Vec2f{float(z.real()), float(z.imag())}, oglplus::Vec3f{float(r), float(g), float(b)}};
+        };
+
+        double b_lenscale = tick * 1;
+        double m_lenscale = 0.1;
+
+
+        buffer.clear();
+        buffer_dots.clear();
+
+        double theta_tick = 2 * PI / division;
+        for (int iy = -field_division_half; iy <= field_division_half; iy++) {
+            for (int ix = -field_division_half; ix <= field_division_half; ix++) {
+                std::complex<double> z = {ix * tick, iy * tick};
+                std::complex<double> b = calcField(dipoles, z);
+                b /= std::abs(b);
+                push_line(vertex(z, 1.0, 0.0, 0.0),
+                    vertex(z + b_lenscale * b, 1.0, 1.0, 0.0));
+            }
+        }
+
+
+        for (auto& dipole : dipoles) {
+            if (std::abs(dipole.z) > 1)
+                continue;
+            push_line(vertex(dipole.z, 0, 0, 1), vertex(dipole.z + (-1.0i) * m_lenscale * dipole.m, 0.8, 0.8, 1));
+        }
+
+        for (auto& dipole : estimated) {
+            push_line(vertex(dipole.z, 0, 0.5, 0), vertex(dipole.z + (-1.0i) * m_lenscale * dipole.m, 0.5, 1, 0.5));
+        }
+
+
+        for (int i = 0; i < division; i++) {
+            std::complex z = std::exp(std::complex<double>{0, i * theta_tick});
+            std::complex z2 = std::exp(std::complex<double>{0, (i + 1) * theta_tick});
+            push_line(vertex(z, 1.0, 0.0, 0.0), vertex(z2, 1.0, 0.0, 0.0));
+            double v = data[i].p.imag();
+            push_dot(vertex(data[i].z, v / (2 * voltage_range) + 0.5, v / (2 * voltage_range) + 0.5, -v / (2 * voltage_range) + 0.5));
+        }
+
+        va->set(buffer.data(), buffer.size());
+        va_dots->set(buffer_dots.data(), buffer_dots.size());
+    };
     glarea.signal_render().connect([&](const Glib::RefPtr<Gdk::GLContext>& /*context*/) {
         glarea.make_current();
         glarea.throw_if_error();
+        draw();
         auto viewport = oglplus::Context::Viewport();
         double aspect_ratio = double(viewport.Width()) / viewport.Height();
 
@@ -78,6 +141,10 @@ int main()
         shader->setMatrix(m);
         va->draw();
         va_dots->draw();
+
+        if (running) {
+            glarea.queue_draw();
+        }
         return true;
     });
 
@@ -93,14 +160,6 @@ int main()
         false);
 
 
-    auto vertex = [](std::complex<double> z, double r, double g, double b) {
-        return Shader::Vertex{oglplus::Vec2f{float(z.real()), float(z.imag())}, oglplus::Vec3f{float(r), float(g), float(b)}};
-    };
-
-    double b_lenscale = tick * 1;
-    double m_lenscale = 0.1;
-
-
     glarea.signal_realize().connect([&] {
         glarea.make_current();
         glarea.throw_if_error();
@@ -111,60 +170,25 @@ int main()
         va_dots = shader->createVertexArray(oglplus::PrimitiveType::Points, 10);
     });
 
-    double theta_tick = 2 * PI / division;
-    auto draw = [&] {
-        buffer.clear();
-        buffer_dots.clear();
-
-        for (int iy = -field_division_half; iy <= field_division_half; iy++) {
-            for (int ix = -field_division_half; ix <= field_division_half; ix++) {
-                std::complex<double> z = {ix * tick, iy * tick};
-                std::complex<double> b = calcField(dipoles, z);
-                b /= std::abs(b);
-                push_line(vertex(z, 1.0, 0.0, 0.0),
-                    vertex(z + b_lenscale * b, 1.0, 1.0, 0.0));
-            }
-        }
-
-
-        for (auto& dipole : dipoles) {
-            push_line(vertex(dipole.z, 0, 0, 1), vertex(dipole.z + m_lenscale * dipole.m, 0.8, 0.8, 1));
-        }
-
-        for (auto& dipole : estimated) {
-            push_line(vertex(dipole.z, 0, 0.5, 0), vertex(dipole.z + m_lenscale * dipole.m, 0.5, 1, 0.5));
-        }
-
-
-        for (int i = 0; i < division; i++) {
-            std::complex z = std::exp(std::complex<double>{0, i * theta_tick});
-            std::complex z2 = std::exp(std::complex<double>{0, (i + 1) * theta_tick});
-            push_line(vertex(z, 1.0, 0.0, 0.0), vertex(z2, 1.0, 0.0, 0.0));
-            push_dot(vertex(z, 1.0, 0.0, 0.0));
-        }
-
-        va->set(buffer.data(), buffer.size());
-        va_dots->set(buffer_dots.data(), buffer_dots.size());
-        glarea.queue_draw();
-    };
 
     std::chrono::time_point start_time = std::chrono::steady_clock::now();
+
+    std::mutex mutex;
     Glib::signal_timeout().connect([&] {
+        if (!running)
+            return true;
+        std::lock_guard<std::mutex> lock(mutex);
         static bool prev_autoplay = false;
         std::chrono::time_point now = std::chrono::steady_clock::now();
         std::chrono::duration t = now - start_time;
         //        dipoles[0].z = 0.5 * std::exp(double(t.count()) * 1.0e-9i);
         //        dipoles[1].z = 0.7 * std::exp(-1.22 * double(t.count()) * 1.0e-9i);
 
-        auto data = potentialTable(dipoles, division);
-        //        for(int i=0;i<data.size();i++){
-        //            std::cout<<i<<": "<<data[i]<<"\n";
-        //        }
-        estimated = estimate(data, 11);
-
+        data = potentialTable(dipoles, division);
         for (int i = 0; i < data.size(); i++) {
-            std::cout << i << ":" << data[i].p << std::endl;
+            data[i].p += std::complex<double>{0, distrib(rng) * std::exp(ln10 * log_noise)};
         }
+        estimated = estimate(data, max_pow);
 
         std::cout << "===\n";
         for (Dipole& d : estimated) {
@@ -173,34 +197,84 @@ int main()
         std::cout << "===\n"
                   << std::endl;
 
-        draw();
         return true;
     },
-        10);
-    glarea.add_events(Gdk::POINTER_MOTION_MASK | Gdk::BUTTON_PRESS_MASK | Gdk::SCROLL_MASK | Gdk::SMOOTH_SCROLL_MASK);
-    glarea.signal_button_press_event().connect([&](GdkEventButton* e) {
+        30, Glib::PRIORITY_LOW);
+    glarea.add_events(Gdk::POINTER_MOTION_MASK | Gdk::BUTTON_PRESS_MASK | Gdk::BUTTON_RELEASE_MASK | Gdk::KEY_PRESS_MASK | Gdk::SCROLL_MASK | Gdk::SMOOTH_SCROLL_MASK);
+    //    glarea.signal_button_press_event().connect([&](GdkEventButton* e) {
+    //
+    //    });
+
+    std::complex<double> mag;
+    mag = {distrib(rng), distrib(rng)};
+    glarea.signal_button_release_event().connect([&](GdkEventButton* e) {
         dipoles.push_back(dipoles[0]);
         dipoles.push_back(dipoles[1]);
-        dipoles[0].m = {distrib(rng), distrib(rng)};
-        return false;
+        mag = {distrib(rng), distrib(rng)};
+        return true;
+    });
+    auto builder = Gtk::Builder::create_from_file("../conf_window.glade");
+    Gtk::Window* window_conf = nullptr;
+    builder->get_widget("window_conf", window_conf);
+
+    Gtk::ToggleButton* btn_run = nullptr;
+    builder->get_widget("btn_run", btn_run);
+
+    btn_run->signal_toggled().connect([&] {
+        active = btn_run->get_active();
     });
 
-    glarea.signal_motion_notify_event().connect([&](GdkEventMotion* e) {
-        static std::complex<double> last_pointer_position = 0;
-        if (glarea.get_height() == 0)
-            return false;
-        std::complex<double> current_pointer_position
-            = {((2 * e->x - glarea.get_width()) / (glarea.get_height())) * field_range_half,
-                (2 * e->y - glarea.get_height()) / (-glarea.get_height()) * field_range_half};
-        //        Vector2d diff = (current_pointer_position - last_pointer_position) / glarea->get_height();
-        /*
+    Gtk::Button* btn_clear = nullptr;
+    builder->get_widget("btn_clear", btn_clear);
+
+    btn_clear->signal_clicked().connect([&] {
+        dipoles.resize(2);
+        mag = {distrib(rng), distrib(rng)};
+    });
+    glarea.signal_motion_notify_event()
+        .connect([&](GdkEventMotion* e) {
+            static std::complex<double> last_pointer_position = 0;
+            if (glarea.get_height() == 0)
+                return false;
+            std::complex<double> current_pointer_position
+                = {((2 * e->x - glarea.get_width()) / (glarea.get_height())) * field_range_half,
+                    (2 * e->y - glarea.get_height()) / (-glarea.get_height()) * field_range_half};
+            //        Vector2d diff = (current_pointer_position - last_pointer_position) / glarea->get_height();
+            /*
         last_pointer_position = current_pointer_position;
          */
 
-        dipoles[0].z = current_pointer_position;
-        dipoles[1] = dipoles[0].conjugate();
+            if (active || (e->state & GDK_BUTTON1_MASK)) {
+                if (std::abs(current_pointer_position) > 1) {
+                    dipoles[0].m = 0;
+                } else {
+                    dipoles[0] = {current_pointer_position, mag};
+                }
+                dipoles[1] = dipoles[0].conjugate();
+            } else {
+                dipoles[0].m = dipoles[1].m = 0;
+            }
+            return true;
+        });
+
+    auto key_press = [&](GdkEventKey* e) {
+        if (e->keyval == 'a') {
+            active = !active;
+            btn_run->set_active(active);
+        } else if (e->keyval == 'r') {
+            running = !running;
+            glarea.queue_draw();
+        } else if (e->keyval == 'c') {
+
+            dipoles.resize(2);
+            mag = {distrib(rng), distrib(rng)};
+        }
         return true;
-    });
+    };
+    window.signal_key_press_event().connect(key_press);
+    window_conf->signal_key_press_event().connect(key_press);
+
+    btn_run->set_active(active);
     /*glarea->signal_scroll_event().connect([&](GdkEventScroll* e) {
         constexpr double scroll_scale = 0.05;
         if (e->state == 0) {
@@ -217,8 +291,29 @@ int main()
     window_params->show();
     //    window_result->show();
     */
+
+
+    auto connect_adjustment = [&](const char* str, auto& var) {
+        Glib::RefPtr<Gtk::Adjustment> adj = Glib::RefPtr<Gtk::Adjustment>::cast_dynamic(builder->get_object(str));
+        adj->signal_value_changed().connect([&] {
+            var = (std::decay_t<decltype(var)>)adj->get_value();
+        });
+        var = (std::decay_t<decltype(var)>)adj->get_value();
+        return adj;
+    };
+    auto adj_div = connect_adjustment("adj_div", division);
+    auto adj_pow = connect_adjustment("adj_pow", max_pow);
+    auto adj_threshold = connect_adjustment("adj_threshold", log_threshold);
+    auto adj_noise = connect_adjustment("adj_noise", log_noise);
+    auto adj_color_range = connect_adjustment("adj_color_range", voltage_range);
+    //    connect_adjustment("adj_color_range", voltage_range);
+
+
+    window_conf->show();
     glarea.show();
     window.show();
     app->run(window);
+
+
     return 0;
 }
